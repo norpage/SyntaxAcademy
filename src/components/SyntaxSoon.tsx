5d09b9f59995957354b60ad32919873d7f154a18
David <davidmeloyan99@gmail.com>
2025-07-04 12:15:58 +0400

    logo

--- src/components/SyntaxSoon.tsx
+++ src/components/SyntaxSoon.tsx
@@ -1,10 +1,19 @@
 'use client';
 import {motion} from 'framer-motion';
+import Image from "next/image";
+import React from "react";
 
 export default function SyntaxSoon() {
     return (
-        <section className="relative w-full min-h-screen flex items-center justify-center bg-gradient-to-br from-[#0f172a] via-[#1e293b] to-[#0f172a] text-white overflow-hidden px-6">
-            <div className="max-w-4xl text-center space-y-6">
+        <section className="relative w-full min-h-screen flex items-center justify-center bg-gradient-to-br from-[#0f172a] via-[#677893] to-[#0f172a] text-white overflow-hidden px-6">
+            <div className="max-w-4xl text-center flex flex-col items-center justify-center space-y-6">
+                <Image
+                    src="/synt_png.png"
+                    width={1000}
+                    height={800}
+                    alt="Logo"
+                    className="relative z-20 w-1/3 h-auto object-cover"
+                />
                 <motion.h1
                     initial={{opacity: 0, y: -30}}
                     animate={{opacity: 1, y: 0}}
